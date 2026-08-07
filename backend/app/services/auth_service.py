a4a2c14f35cee39ff2ca6d862152aacc72b55f23
siddeshh <siddeshhiwale82@gmail.com>
2026-08-08 02:06:26 +0530

    feat: implement authentication service and schemas

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,7 +1,60 @@
-# register user
+from sqlalchemy.orm import Session
 
-#login user
+from app.models.user import User
+from app.schemas.user import UserCreate
+from app.core.security import hash_password, verify_password
+from app.core.auth import create_access_token
 
-#verify credentials
+#Register User
+def register_user(db: Session, user: UserCreate):
 
-#generate JWT
+    existing_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if existing_user:
+        raise ValueError("Email already registered")
+
+    new_user = User(
+        full_name=user.full_name,
+        email=user.email,
+        password_hash=hash_password(user.password),
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+#Login User
+def login_user(
+    db: Session,
+    email: str,
+    password: str,
+):
+
+    user = (
+        db.query(User)
+        .filter(User.email == email)
+        .first()
+    )
+
+    if not user:
+        raise ValueError("Invalid email or password")
+
+    if not verify_password(
+        password,
+        user.password_hash,
+    ):
+        raise ValueError("Invalid email or password")
+
+    token = create_access_token(
+        {
+            "sub": user.email
+        }
+    )
+
+    return token
